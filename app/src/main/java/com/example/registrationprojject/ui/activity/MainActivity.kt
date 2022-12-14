90d3f9cb0721f0bcb46ace7a59f5aa4585df5991
Suiunbek02 <abasbekovsuiunbek@gmail.com>
2022-12-14 19:03:31 +0600

    I have completed my homework

--- app/src/main/java/com/example/registrationprojject/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/registrationprojject/ui/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.registrationprojject.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
